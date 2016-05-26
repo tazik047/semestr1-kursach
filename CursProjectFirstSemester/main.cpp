39d6aeed06ec0086672c1f849a4dd54f6ff7b46e
Станислав <stas249501@gmail.com>
2016-05-26 23:25:14 +0300

    фвыфыв

--- CursProjectFirstSemester/main.cpp
+++ CursProjectFirstSemester/main.cpp
@@ -59,7 +59,8 @@ int main()
 		std::cout << "3. Быстрая сортировка" << std::endl;
 		std::cout << "4. С помощью кучи" << std::endl;
 		std::cout << "5. Распечатать текущий массив" << std::endl;
-		std::cout << "6. Очистить экран" << std::endl;
+		std::cout << "6. Сгенерировать массив заданной длины" << std::endl;
+		std::cout << "7. Очистить экран" << std::endl;
 		std::cout << "0. Выйти" << std::endl;
 		std::cin >> command;
 		switch (command)
@@ -106,6 +107,8 @@ int main()
 			}
 			break;
 		case 6:
+			break;
+		case 7:
 			system("cls");
 			break;
 		}
@@ -115,3 +118,80 @@ int main()
 
 	return 0;
 }
+/*
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void iswap(int &n1, int &n2)
+{
+int temp = n1;
+n1 = n2;
+n2 = temp;
+}
+
+int main()
+{
+int const n = 100;
+int a[n];
+for ( int i = 0; i < n; ++i ) { a[i] = n - i; cout << a[i] << " "; }
+//заполняем массив для наглядности.
+//-----------сортировка------------//
+//сортирует по-возрастанию. чтобы настроить по-убыванию,
+//поменяйте знаки сравнения в строчках, помеченных (знак)
+int sh = 0; //смещение
+bool b = false;
+for (;;)
+{
+	b = false;
+	for (int i = 0; i < n; i++)
+	{
+		if (i * 2 + 2 + sh < n)
+		{
+			if ((a[i + sh] >  a[i * 2 + 1 + sh]) || (a[i + sh] >  a[i * 2 + 2 + sh]))
+			{
+				if (a[i * 2 + 1 + sh] <  a[i * 2 + 2 + sh])
+				{
+					iswap(a[i + sh], a[i * 2 + 1 + sh]);
+					b = true;
+				}
+				else if (a[i * 2 + 2 + sh] <  a[i * 2 + 1 + sh])
+				{
+					iswap(a[i + sh], a[i * 2 + 2 + sh]);
+					b = true;
+				}
+			}
+			//дополнительная проверка для последних двух элементов
+			//с помощью этой проверки можно отсортировать пирамиду
+			//состоящую всего лишь из трех элементов
+			if (a[i * 2 + 2 + sh] <  a[i * 2 + 1 + sh])
+			{
+				iswap(a[i * 2 + 1 + sh], a[i * 2 + 2 + sh]);
+				b = true;
+			}
+		}
+		else if (i * 2 + 1 + sh < n)
+		{
+			if (a[i + sh] >  a[i * 2 + 1 + sh])
+			{
+				iswap(a[i + sh], a[i * 2 + 1 + sh]);
+				b = true;
+			}
+		}
+	}
+	if (!b) sh++; //смещение увеличивается, когда на текущем этапе
+	//сортировать больше нечего
+	if (sh + 2 == n) break;
+}  //конец сортировки
+
+
+cout << endl << endl;
+for (int i = 0; i < n; ++i) cout << a[i] << " ";
+
+
+getch();
+return 0;
+}
+
+*/
