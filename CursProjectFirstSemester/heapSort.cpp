70eb53775f49978b28a98cf38ca05c9c0d51913a
Станислав <stas249501@gmail.com>
2016-06-22 06:01:06 +0300

    fix heap sort

--- CursProjectFirstSemester/heapSort.cpp
+++ CursProjectFirstSemester/heapSort.cpp
@@ -12,8 +12,8 @@ void downHeap(int a[], int k, int n) {
 	int child;
 	new_elem = a[k];
 
-	while (k <= n / 2) {  		// пока у a[k] есть дети 
-		child = 2 * k;
+	while (2*k + 1 < n) {  		// пока у a[k] есть дети 
+		child = 2 * k + 1;
 		//  выбираем большего сына 
 		if (child < n && a[child] < a[child + 1])
 			child++;
